d79dd99be8b460b33db97c42ed0ef3852fe6593e
bamkhatri <bamkhatri24@gmail.com>
2022-01-11 10:39:20 +0545

    modified detail

--- src/component/detail/index.js
+++ src/component/detail/index.js
@@ -7,7 +7,7 @@ function Detail() {
       <div className='background'>
         <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg' />
       </div>
-      <div className='div-image'>
+      <div className='image-title'>
         <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78' />
       </div>
       <div className='controls'>
